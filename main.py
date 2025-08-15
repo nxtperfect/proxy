898f478825621f7c315d4fef48a116f7e6e130f7
NxtPerfect <alakaxan@protonmail.com>
2025-08-15 13:10:44 +0200

    feat: added reverse proxy class
    wip: working on reverse proxy

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ class Proxy:
                 if not len(reply):
                     break
                 _ = conn.send(reply)
+                print("Request sent successfully.")
         except socket.error:
             print(sock.error)
             exit(1)
@@ -69,8 +70,38 @@ class Proxy:
             conn.close()
 
 
-def reverse_proxy():
-    pass
+@dataclass
+class ReverseProxy:
+    ip: str
+    port: int
+    max_connections: int = 5
+    buffer_size: int = 1024
+    available_servers = ["http://wikipedia.org", "https://medium.com"]
+
+    def start(self):
+        # Connect to client
+        # Get request
+        # Send request to available_servers
+        # Get Request
+        # change headers so it came from proxy
+        # return to client
+        print("Starting new reverse proxy...")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.ip, self.port))
+        self.sock.listen()
+        try:
+            conn, addr = self.sock.accept()
+            data = conn.recv(self.buffer_size)
+            print(data)
+            _ = start_new_thread(self._connection_string, (data, conn, addr))
+        except Exception as e:
+            print(f"Failed to get request. {e}")
+        finally:
+            self.sock.close()
+            exit(1)
+
+    def _connection_string(self):
+        pass
 
 
 def server():
